8747b0c5b00d5844fa24e537d4dd96ef0a208cc9
Parveen <hello@parveen.me>
2023-02-25 06:05:12 -0800

    Added files

---
+++ client/remote-port-forwarding-with-ssh.sh
@@ -0,0 +1,71 @@
+CONFIG_DIR='/etc/remote-port-forwarding-with-ssh'
+MAX_WAIT=600
+port_mapping_conf=$1
+remote_conf=$2
+IFS=$'\n'
+args=''
+for line in `cat $port_mapping_conf`; do
+  line=$(echo $line | tr -d " \t\n\r")
+  if [[ "$line" != *"="* ]]
+  then
+    continue
+  fi
+  key="$( cut -d '=' -f 1 <<< "$line" )";
+  value="$( cut -d '=' -f 2 <<< "$line" )";
+  if [[ (! -z $key) && (! -z $value) ]]
+  then
+    args="$args -R $key:$value"
+  fi
+done
+source $remote_conf
+
+if [ -z $user ]
+then
+  echo "user is not provided in $CONFIG_DIR/remote.conf"
+  exit 1
+fi
+if [ -z $address ]
+then
+  echo "address is not provided in $CONFIG_DIR/remote.conf"
+  exit 1
+fi
+if [ -z $port ]
+then
+  echo "port is not provided in $CONFIG_DIR/remote.conf"
+  exit 1
+fi
+
+if [ -z $ssh_key ]
+then
+  echo "ssh_key is not provided in $CONFIG_DIR/remote.conf"
+  exit 1
+fi
+
+if [ -z $args ]
+then
+  echo "No valid remote port mapping found in $CONFIG_DIR/remote-port-mapping.conf"
+  exit 1
+fi
+
+wait_time=60
+while :
+do
+  start_time=$(date +%s)
+  cmd="ssh -N $args $user@$address -p $port -i $ssh_key"
+  echo "Executing command: $cmd"
+	eval $cmd
+  sleep 31
+  now=$(date +%s)
+  diff=$((now - start_time))
+  if [ $diff -gt 600 ]
+  then
+    wait_time=60
+  fi
+  echo "Will try again after $wait_time seconds"
+  sleep $wait_time
+  wait_time=$((wait_time * 2))
+  if [ $wait_time -gt $MAX_WAIT ]
+  then
+    wait_time=$MAX_WAIT
+  fi
+done
---
+++ client/setup.sh
@@ -0,0 +1,30 @@
+fail() {
+    echo "Execution failed. $1"
+    exit 1
+}
+
+CONFIG_DIR='/etc/remote-port-forwarding-with-ssh'
+
+[ -d $CONFIG_DIR ] || mkdir $CONFIG_DIR || fail 'Not able to create dir.'
+
+[ -f "remote-port-forwarding-with-ssh.sh" ] || fail "remote-port-forwarding-with-ssh.sh file not found"
+
+cp remote-port-forwarding-with-ssh.sh /usr/sbin/remote-port-forwarding-with-ssh
+chmod +x "/usr/sbin/remote-port-forwarding-with-ssh"
+
+touch "$CONFIG_DIR/remote-port-mapping.conf"
+touch "$CONFIG_DIR/remote.conf"
+
+tee /etc/systemd/system/remote-port-forwarding-with-ssh.service << EOF
+[Unit]
+Description=Remote port forwarding using ssh
+[Install]
+WantedBy=multi-user.target
+[Service]
+Type=simple
+WorkingDirectory=/usr/sbin
+ExecStart=/bin/bash remote-port-forwarding-with-ssh "$CONFIG_DIR/remote-port-mapping.conf" "$CONFIG_DIR/remote.conf"
+StandardOutput=file:/var/log/remote-port-forwarding-with-ssh.log
+StandardError=file:/var/log/remote-port-forwarding-with-ssh.log
+SyslogIdentifier=%n
+EOF
