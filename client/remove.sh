aa8196765096153bc6234474c3bd6e7ce212f464
Parveen Chahal <hello@parveen.me>
2023-11-09 19:29:22 -0800

    Update remove.sh

--- client/remove.sh
+++ client/remove.sh
@@ -2,4 +2,5 @@ systemctl stop remote-port-forwarding-with-ssh.service
 rm /usr/sbin/remote-port-forwarding-with-ssh
 rm -r /etc/remote-port-forwarding-with-ssh
 rm /var/log/remote-port-forwarding-with-ssh.log
+rm /var/log/remote-port-forwarding-with-ssh-err.log
 systemctl daemon-reload
